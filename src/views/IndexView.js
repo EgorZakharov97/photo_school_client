242934d854c2b505dad319b25f62a014bd3243e9
EgorZakharov97 <skymailsenter@gmail.com>
2020-08-23 03:15:13 -0500

    appfairy: Migrate

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -50,16 +50,6 @@ class IndexView extends React.Component {
   render() {
     const proxies = Controller !== IndexView ? transformProxies(this.props.children) : {
       'course-home': [],
-      'name': [],
-      'start-date': [],
-      'description': [],
-      'will-learn': [],
-      'timeline': [],
-      'duration': [],
-      'price': [],
-      'deadline': [],
-      'places': [],
-      'register': [],
       'leave-email': [],
     }
 
@@ -274,73 +264,9 @@ class IndexView extends React.Component {
                 <h2 className="af-class-heading-on-black af-class-modified">Classes <span className="af-class-text-span-2">Calendar</span></h2>
               </div>
               <div className="af-class-container-7 w-container">
-                {map(proxies['course-home'], props => <div {...{...props, className: `af-class-dropdown-wrapper ${props.className || ''}`}}>{createScope(props.children, proxies => <React.Fragment>
+                {map(proxies['course-home'], props => <div {...{...props, className: `af-class-dropdown-wrapper ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
                   <CourseHomeView.Controller />
-                  <div className="af-class-dropdown af-class-hidden">
-                    <div data-w-id="fa70b82d-84c3-d193-a03e-ced5befaa3b4" className="af-class-dropdown-trigger">
-                      {map(proxies['name'], props => <div {...{...props, className: `af-class-text-block ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Course Name</React.Fragment>}</div>)}
-                      {map(proxies['start-date'], props => <div {...{...props, className: `af-class-date ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Aug 20</React.Fragment>}</div>)}
-                    </div>
-                    <div style={{height: '0PX', display: 'block'}} className="af-class-dropdown-content">
-                      <div className="af-class-cladd-desc-wrapper">
-                        <div className="w-layout-grid af-class-grid_course_home">
-                          <div id="w-node-ced5befaa3bc-fbba94a3" className="af-class-course-card">
-                            {map(proxies['description'], props => <div {...{...props, className: `af-class-rich-text-block-7 w-richtext ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
-                              <h2>What’s a Rich Text element?</h2>
-                              <p>The rich text element allows you to create and format headings, paragraphs, blockquotes, images, and video all in one place instead of having to add and format them individually. Just double-click and easily create content.</p>
-                              <h4>Static and dynamic content editing</h4>
-                              <p>A rich text element can be used with static or dynamic content. For static content, just drop it into any page and begin editing. For dynamic content, add a rich text field to any collection and then connect a rich text element to that field in the settings panel. Voila!</p>
-                              <h4>How to customize formatting for each rich text</h4>
-                              <p>Headings, paragraphs, blockquotes, figures, images, and figure captions can all be styled after a class is added to the rich text element using the "When inside of" nested selector system.</p>
-                            </React.Fragment>}</div>)}
-                          </div>
-                          <div id="w-node-ced5befaa3ca-fbba94a3" className="af-class-course-card">
-                            {map(proxies['will-learn'], props => <div {...{...props, className: `af-class-rich-text-block-7 w-richtext ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
-                              <h2>What’s a Rich Text element?</h2>
-                              <p>The rich text element allows you to create and format headings, paragraphs, blockquotes, images, and video all in one place instead of having to add and format them individually. Just double-click and easily create content.</p>
-                              <h4>Static and dynamic content editing</h4>
-                              <ul role="list">
-                                <li>A rich text element can be used with static or dynamic content.</li>
-                                <li>A rich text element can be used with static or dynamic content.</li>
-                                <li>A rich text element can be used with static or dynamic content.</li>
-                                <li>A rich text element can be used with static or dynamic content.</li>
-                              </ul>
-                              <h4>How to customize formatting for each rich text</h4>
-                              <p>Headings, paragraphs, blockquotes, figures, images, and figure captions can all be styled after a class is added to the rich text element using the "When inside of" nested selector system.</p>
-                            </React.Fragment>}</div>)}
-                          </div>
-                          <div id="w-node-ced5befaa3df-fbba94a3" className="af-class-course-card">
-                            {map(proxies['timeline'], props => <div {...{...props, className: `af-class-rich-text-block-7 w-richtext ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
-                              <h2>What’s a Rich Text element?</h2>
-                              <p>The rich text element allows you to create and format headings, paragraphs, blockquotes, images, and video all in one place instead of having to add and format them individually. Just double-click and easily create content.</p>
-                              <h4>Static and dynamic content editing</h4>
-                              <p>A rich text element can be used with static or dynamic content. For static content, just drop it into any page and begin editing. For dynamic content, add a rich text field to any collection and then connect a rich text element to that field in the settings panel. Voila!</p>
-                              <h4>How to customize formatting for each rich text</h4>
-                              <p>Headings, paragraphs, blockquotes, figures, images, and figure captions can all be styled after a class is added to the rich text element using the "When inside of" nested selector system.</p>
-                            </React.Fragment>}</div>)}
-                          </div>
-                          <div id="w-node-ced5befaa3ed-fbba94a3" className="af-class-course-card af-class-dates">
-                            <div className="af-class-dateblock">
-                              <div className="af-class-date-phrase">Duration</div>
-                              {map(proxies['duration'], props => <div {...{...props, className: `af-class-date-duration af-class-red ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>NAN</React.Fragment>}</div>)}
-                            </div>
-                            <div className="af-class-dateblock">
-                              <div className="af-class-date-phrase">Price</div>
-                              {map(proxies['price'], props => <div {...{...props, className: `af-class-date-duration af-class-red ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>NAN</React.Fragment>}</div>)}
-                            </div>
-                            <div className="af-class-dateblock">
-                              <div className="af-class-date-phrase">Registration deadline</div>
-                              {map(proxies['deadline'], props => <div {...{...props, className: `af-class-date-duration ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>NAN</React.Fragment>}</div>)}
-                            </div>
-                            <div className="af-class-dateblock">
-                              <div className="af-class-date-phrase">Places left</div>
-                              {map(proxies['places'], props => <div {...{...props, className: `af-class-date-duration ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>NAN</React.Fragment>}</div>)}
-                            </div>
-                          </div>{map(proxies['register'], props => <a id="w-node-ced5befaa402-fbba94a3" href="#" {...{...props, className: `af-class-cors-register-now-2 af-class-courses ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Register now</React.Fragment>}</a>)}</div>
-                      </div>
-                    </div>
-                  </div>
-                </React.Fragment>)}</div>)}
+                </React.Fragment>}</div>)}
               </div>
             </div>
             <div id="contact" className="af-class-section-ask">
