a056d9d05706dfb40443d242604111603be96283
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-01 04:12:02 -0500

    appfairy: Migrate

--- src/views/AdminView.js
+++ src/views/AdminView.js
@@ -13,8 +13,6 @@ const scripts = [
   fetch("https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f1212b6860f150f9f0e6e14").then(body => body.text()),
   fetch("js/webflow.js").then(body => body.text()),
   Promise.resolve("$(\".tab-button\").click(function(t){t.preventDefault(),$(\".tab-button\").removeClass(\"tab-button-active\"),$(\".w-tab-link:contains(\"+t.target.innerText+\")\").click(),$(t.target).addClass(\"tab-button-active\")}),$(\".af-class-tab-button\").click(function(t){t.preventDefault(),$(\".af-class-tab-button\").removeClass(\"af-class-tab-button-active\"),$(\".w-tab-link:contains(\"+t.target.innerText+\")\").click(),$(t.target).addClass(\"af-class-tab-button-active\")});"),
-  fetch("https://cdn.quilljs.com/1.3.6/quill.js").then(body => body.text()),
-  Promise.resolve("var description=new Quill(\"#wk-description\",{theme:\"snow\"}),willLearn=new Quill(\"#wk-willLearn\",{theme:\"snow\"}),timeline=new Quill(\"#wk-timeline\",{theme:\"snow\"});"),
 ]
 
 let Controller
