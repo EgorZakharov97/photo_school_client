7c6ff1cd5520c0014c51a283f982b8b54b175955
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-01 03:45:20 -0500

    s

--- src/controllers/AdminWorkshopsController.js
+++ src/controllers/AdminWorkshopsController.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Script from 'react-load-script'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import AdminWorkshopView from '../views/AdminWorkshopsView'
 
 export default class AdminWorkshopsController extends React.Component {
@@ -7,7 +8,8 @@ export default class AdminWorkshopsController extends React.Component {
     state = {
         displayFile: true,
         quill: false,
-        workshopList: []
+        workshopList: [],
+        workshop: {}
     }
 
     componentDidMount(){
@@ -16,7 +18,13 @@ export default class AdminWorkshopsController extends React.Component {
             return res.json()
         })
         .then(data => {
-            this.setState({workshopList: data.data})
+            if(data.success){
+                this.setState(state => {
+                    state.workshopList = data.data
+                })
+            } else {
+                console.log(data.error)
+            }
         })
     }
 
@@ -29,28 +37,59 @@ export default class AdminWorkshopsController extends React.Component {
                         return <option value={workshop.name}>{workshop.name}</option>
                     }) }
                 </selector>
-                <name/>
-                <starts/>
-                <ends/>
-                <deadline/>
-                <duration/>
-                <price/>
-                <seats/>
+                <name value={this.state.workshop.name} onChange={this.setName.bind(this)}/>
+                <starts value={this.state.workshop.starts} onChange={this.setStarts.bind(this)}/>
+                <ends value={this.state.workshop.ends} onChange={this.setEnds.bind(this)} />
+                <deadline value={this.state.workshop.deadline} onChange={this.setDeadline.bind(this)} />
+                <duration value={this.state.workshop.duration} onChange={this.setDuration.bind(this)} />
+                <price value={this.state.workshop.price} onChange={this.setPrice.bind(this)} />
+                <seats value={this.state.workshop.seats} onChange={this.setSeats.bind(this)} />
                 { this.state.displayFile && <file/> }
-                <description></description>
-                <timeline></timeline>
-                <willLearn></willLearn>
-                <submit>{this.state.workshopList.lenath > 0 ? "Update" : "Create"}</submit>
-                <Script 
-                    url="https://cdn.quilljs.com/1.3.6/quill.js"
-                    onCreate={this.loadQuill.bind(this)}
-                />
+                <description>
+                    <ReactQuill theme="snow" value={this.state.workshop.description} onChange={this.setDescription.bind(this)}/>
+                </description>
+                <timeline>
+                    <ReactQuill theme="snow" value={this.state.workshop.timeline} onChange={this.setTimeline.bind(this)}/>
+                </timeline>
+                <will-learn>
+                    <ReactQuill theme="snow" value={this.state.workshop.willLearn} onChange={this.setWillLearn.bind(this)}/>
+                </will-learn>
+                <submit value={this.state.workshopList.lenath > 0 ? "Update" : "Create"} onClick={this.submitForm.bind(this)}></submit>
             </AdminWorkshopView>
         )
     }
 
-    loadQuill() {
-        this.setState({quill: true})
+    setDescription(val) {
+        this.setState(state => {
+            state.workshop.description = val;
+        })
+    }
+
+    setTimeline(val) {
+        this.setState(state => {
+            state.workshop.Timeline = val;
+        })
+    }
+
+    setWillLearn(val) {
+        this.setState(state => {
+            state.workshop.willLearn = val;
+        })
+    }
+
+    submitForm(e){
+        e.preventDefault();
+        fetch('/api/v1/admin/workshop', {
+            method: 'post',
+            body: JSON.stringify(this.state)
+        })
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        console.log(this.state)
     }
 
     selectorChange(name) {
@@ -64,45 +103,77 @@ export default class AdminWorkshopsController extends React.Component {
     }
 
     setWorkshop(workshop) {
-        this.setName(workshop.name);
-        this.setSeats(workshop.importantDates.courseStarts)
-        this.setEnds(workshop.importantDates.courseEnds)
-        this.setDeadline(workshop.importantDates.registrationDeadline)
-        this.setDuration(workshop.duration)
-        this.setPrice(workshop.pricing.finalPrice)
-        this.setSeats(workshop.seats.total)
-        this.setState({displayFile: false})
+        if(workshop === "new"){
+            this.setState(state => {
+                state.workshop = {}
+                state.displayFile = true
+            })
+        } else {
+            this.setState(state => {
+                state.workshop.name = workshop.name;
+                state.workshop.starts = workshop.importantDates.courseStarts;
+                state.workshop.ends = workshop.importantDates.courseEnds
+                state.workshop.deadline = workshop.importantDates.registrationDeadline
+                state.workshop.duration = workshop.duration
+                state.workshop.price = workshop.pricing.finalPrice
+                state.workshop.seats = workshop.seats.total
+            })
+        }
     }
 
-    setName(name) {
-        this.setState({name})
+    setName(e) {
+        let name = e.target.value
+        this.setState(state => {
+            state.workshop.duration = name
+        })
     }
 
-    setStarts(starts) {
-        this.setState({starts})
+    setStarts(e) {
+        let date = new Date(e.target.value)
+        this.setState(state => {
+            state.workshop.starts = date
+        })
     }
 
-    setEnds(ends) {
-        this.setState({ends})
+    setEnds(e) {
+        let date = new Date(e.target.value)
+        this.setState(state => {
+            state.workshop.ends = date
+        })
     }
 
-    setDeadline(deadline) {
-        this.setState({deadline})
+    setDeadline(e) {
+        let date = new Date(e.target.value)
+        this.setState(state => {
+            state.workshop.deadline = date
+        })
     }
 
-    setDuration(duration) {
-        this.setState({duration})
+    setDuration(e) {
+        let duration = e.target.value
+        this.setState(state => {
+            state.workshop.duration = duration
+        })
     }
 
-    setPrice(price) {
-        this.setState({price})
+    setPrice(e) {
+        let price = e.target.value
+        this.setState(state => {
+            state.workshop.price = price
+        })
     }
 
-    setSeats(seats) {
-        this.setState({seats})
+    setSeats(e) {
+        let seats = e.target.value
+        this.setState(state => {
+            state.workshop.seats = seats
+        })
     }
 
-    setFile(file) {
-        this.setState({file})
+    setFile(e) {
+        let file = e.target.value
+        this.setState(state => {
+            state.workshop.file = file
+        })
     }
 }
