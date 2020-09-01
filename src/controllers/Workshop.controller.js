3b225003ca19ab2d418bbc349608a1c7ccbb6af3
EgorZakharov97 <skymailsenter@gmail.com>
2020-08-31 22:10:13 -0500

    s

--- src/controllers/Workshop.controller.js
+++ src/controllers/Workshop.controller.js
@@ -6,7 +6,15 @@ export default class Workshop extends React.Component {
 
     render() {
         return (
-            <WorkshopView/>
+            <WorkshopView>
+                <name>Course</name>
+                <starts>Date</starts>
+                <duration>5 hours</duration>
+                <price>$10</price>
+                <discount>%10</discount>
+                <deadline>date</deadline>
+                <places>10</places>
+            </WorkshopView>
         )
     }
 }
