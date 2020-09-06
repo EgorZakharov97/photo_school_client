eb998b2468299523eb61b409f96644b52d8687cf
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-06 04:04:27 -0500

    s

--- src/controllers/AdminCoursesController.js
+++ src/controllers/AdminCoursesController.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import AdmiCourseView from '../views/AdminCo'
+import AdmiCourseView from '../views/AdminC'
 import AdminFormController from './AdminFormController'
 import {URL_GET_CHALLENGE_NAMES, URL_GET_CHALLENGE_DATA, URL_POST_CHALLENGE} from '../constants'
 import AdminChallengesView from '../views/AdminChallengesView'
