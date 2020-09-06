4efd759071366217c5134f994925b74f99277261
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-05 19:08:18 -0500

    s

--- src/controllers/AdminCouponsController.js
+++ src/controllers/AdminCouponsController.js
@@ -6,42 +6,111 @@ import CouponsContainerView from '../views/CouponsContainerView'
 import CouponView from '../views/CouponView'
 import {URL_GET_COUPONS, URL_POST_COUPON, URL_GET_COUPON_DATA} from '../constants'
 
-export default class AdminCouponController extends AdminFormController {
-    constructor(props) {
-        super(props)
-        this.URL_GET_LIST = URL_GET_COUPONS
-        this.URL_POST_OBJECT = URL_POST_COUPON
-        this.URL_GET_OBJECT_DATA = URL_GET_COUPON_DATA
+export default class AdminCouponController extends React.Component {
+
+    state = {
+        data: {
+            shouldExpire: false,
+        },
+        coupons: [],
+        message: {},
+    }
+
+    componentDidMount(){
+        axios.get(URL_GET_COUPONS)
+        .then(res => {
+            let data = res.data;
+            if(data.success){
+                this.setState({coupons: data.body || []})
+            } else {
+                console.log(data.message)
+            }
+        })
+        .catch(e => {
+            console.log(e)
+        })
     }
 
     render(){
+        console.log(this.state.coupons)
         return (
             <div>
                 <CouponsFormView>
-                    <name/>
-                    <discount/>
-                    <code/>
-                    <product/>
-                    <ifExpires/>
-                    <expieryBlock/>
-                    <expires/>
-                    <once/>
-                    <unlimited/>
-                    <submit/>
-                    <message/>
+                    <name value={this.state.data.name || ""} onChange={e => this.onChange(e)}/>
+                    <discount value={this.state.discount || ""} onChange={e => this.onChange(e)}/>
+                    <code value={this.state.code || ""} onChange={e => this.onChange(e)}/>
+                    <product value={this.state.product || ""} onChange={e => this.onChange(e)}/>
+                    <shouldExpire checked={this.state.data.ifExpires} value={this.state.data.ifExpires} onChange={this.onShouldExpire.bind(this)} />
+                    {this.state.data.ifExpires && <expieryBlock/>}
+                    <expires value={this.state.data.expires} onChange={e => this.onExpiryDateChange(e)} />
+                    <once value={this.state.data.usage} onChange={e => this.onUsageChange(e)} />
+                    <unlimited value={this.state.data.usage} onChange={e => this.onUsageChange(e)} />
+                    <submit onClick={this.submit()} />
+                    {this.state.message.body && <message style={this.state.message.positive ? {color: "green"} : {color: "red"}}>{this.state.message.body}</message>}
                 </CouponsFormView>
-                <coupons-list>
-                    <coupon>
-                        <CouponView>
-                            
-                        </CouponView>
-                    </coupon>
-                </coupons-list>
-            </div>
-            
                 
-
-                    
+                <coupons-container>
+                    <CouponsContainerView>
+                        {this.state.coupons.map((coupon, i) => {
+                            return (
+                                <coupon key={i}>
+                                    <CouponView key={i}>
+                                        <name>{coupon.name}</name>
+                                        <code>{coupon.code}</code>
+                                        <product>{}</product>
+                                        <usage>{}</usage>
+                                        {/* <expires>{new Date(coupon.expiryDate).toISOString()}</expires> */}
+                                        <discount>{coupon.discount}%</discount>
+                                        <used>{coupon.wasUsed}</used>
+                                        <delete>{}</delete>
+                                    </CouponView>
+                                </coupon>
+                            )
+                        })}
+                    </CouponsContainerView>
+                </coupons-container>
+            </div>      
         )
+    }
+
+    submit(){
+
+    }
+
+    setMessage(message, positive=true){
+        this.setState(state => {
+            return state.message = {
+                positive: positive,
+                body: message || "Unknown event occured"
+            }
+        })
+    }
+
+    onUsageChange(e) {
+        const value = e.target.value;
+        this.setState(state => {
+            return state.data.usage = value
+        })
+    }
+
+    onExpiryDateChange(e){
+        const date = e.target.value;
+        this.setState(state => {
+            return state.data.expires = date
+        })
+    }
+
+    onShouldExpire(){
+        this.setState(state => {
+            return state.data.ifExpires = !state.data.ifExpires
+        })
+    }
+
+    changeHandler(e){
+        let name = e.target.name
+        let value = e.target.value
+        this.setState(state => {
+            return state.data[name] = value
+        })
     }
 }
