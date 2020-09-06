91d23a21e8827c9c0e39ca6c80c9720a90c3c6dc
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-06 01:33:37 -0500

    appfairy: Migrate

--- src/views/AdminChallengesView.js
+++ src/views/AdminChallengesView.js
@@ -46,6 +46,7 @@ class AdminChallengesView extends React.Component {
 
   render() {
     const proxies = Controller !== AdminChallengesView ? transformProxies(this.props.children) : {
+      'selector': [],
       'name': [],
       'file': [],
       'description': [],
@@ -61,7 +62,7 @@ class AdminChallengesView extends React.Component {
           @import url(/css/photolite-academy.webflow.css);
         ` }} />
         <span className="af-view">
-          <form id="wf-form-courseForm" name="wf-form-courseForm" data-name="courseForm" method="post" encType="multipart/form-data" className="af-class-form-5"><label htmlFor="name-12">Challenge name</label>{map(proxies['name'], props => <input type="text" maxLength={256} name="name" data-name="name" placeholder="Challenge Name" id="name-12" required {...{...props, className: `w-input ${props.className || ''}`}}>{props.children}</input>)}<label>Challenge preview image</label>
+          <form id="wf-form-courseForm" name="wf-form-courseForm" data-name="courseForm" method="post" encType="multipart/form-data" className="af-class-form-5"><label htmlFor="name-12">Select challenge</label>{map(proxies['selector'], props => <select id="selector-2" name="selector-2" data-name="Selector 2" {...{...props, className: `w-select ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment><option value="!new">New</option></React.Fragment>}</select>)}<label htmlFor="name-13">Challenge name</label>{map(proxies['name'], props => <input type="text" maxLength={256} name="name" data-name="name" placeholder="Challenge Name" id="name-12" required {...{...props, className: `w-input ${props.className || ''}`}}>{props.children}</input>)}<label>Challenge preview image</label>
             {map(proxies['file'], props => <div {...{...props, className: `af-class-html-embed-2 w-embed ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment><input type="file" name="image" placeholder="Choose Image" /></React.Fragment>}</div>)}<label>Challenge Description</label>
             {map(proxies['description'], props => <div id="wk-description" {...{...props, className: `af-class-editor ${props.className || ''}`}}>{props.children}</div>)}
             <div className="af-class-submission">{map(proxies['submit'], props => <input type="submit" value="Create" data-wait="Please wait..." id="submit" {...{...props, className: `af-class-button af-class-red w-button ${props.className || ''}`}}>{props.children}</input>)}
