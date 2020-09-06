f8c58b5ee872c2632fd9215e14795a8bcd87fe98
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-06 01:26:49 -0500

    s

---
+++ src/controllers/AdminChallengesController.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdmiChallengesView from '../views/Admin'
+import AdminFormController from './AdminFormController'
+import {URL_GET_MATERIAL_NAMES, URL_POST_MATERIAL, URL_GET_MATERIAL_DATA} from '../constants'
+
+export default class AdminChallengesController extends AdminFormController {
+
+}
