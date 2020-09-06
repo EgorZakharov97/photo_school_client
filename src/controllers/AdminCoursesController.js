78449e632e1413b0403351ffe551ba32bf5a4eff
EgorZakharov97 <skymailsenter@gmail.com>
2020-09-06 04:02:48 -0500

    s

---
+++ src/controllers/AdminCoursesController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AdmiCourseView from '../views/AdminCo'
+import AdminFormController from './AdminFormController'
+import {URL_GET_CHALLENGE_NAMES, URL_GET_CHALLENGE_DATA, URL_POST_CHALLENGE} from '../constants'
+import AdminChallengesView from '../views/AdminChallengesView'
+
+export default class AdminChallengesController extends AdminFormController {
+
+}
